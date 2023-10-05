a916d904367496a5847b03a5c8b8f1b4404fd6ac
joshikavan92 <joshikavan92@gmail.com>
2023-10-05 11:32:51 +0200

    Update user_message_jamfhelper.sh
    Offboarding Checklist options added

--- user_message_jamfhelper.sh
+++ user_message_jamfhelper.sh
@@ -31,20 +31,32 @@ icon="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertNote
 title="Important Message from IT"
 
 # Create the checklist file
-checklistFile="/Users/$CURRENT_USER/Desktop/Offboarding Checklist.txt"
-echo "Offboarding Checklist:
-1. Back up personal files: Ensure any personal files on the machine are backed up. 
-2. Remove personal accounts: Any personal accounts (like Apple ID, Google, etc.) associated with the system should be removed. 
-3. Return hardware: Any physical items, like security keys or access cards, should be returned.
-4. Transfer work files: If there are files or documents related to work, ensure they are transferred to the appropriate person or place.
-5. Provide contacts: If there are individuals that will need to take over your tasks, please provide this information to your supervisor. 
-6. Prepare the machine: The machine will need to be returned in a state ready for the next user. This includes cleaning the machine, packing it in its original box if available, etc.
-7. Data Security: Please ensure adherence to the data policy while transferring personal data.
 
-Over the next few days, you will receive more information on the above points." > "$checklistFile"
+# Option 1:
+# If you wish you create a simple Text File on Desktop, uncomment till line 46 and comment line 54-55
+# checklistFile="/Users/$CURRENT_USER/Desktop/Offboarding Checklist.txt"
+# echo "Offboarding Checklist:
+# 1. Back up personal files: Ensure any personal files on the machine are backed up. 
+# 2. Remove personal accounts: Any personal accounts (like Apple ID, Google, etc.) associated with the system should be removed. 
+# 3. Return hardware: Any physical items, like security keys or access cards, should be returned.
+# 4. Transfer work files: If there are files or documents related to work, ensure they are transferred to the appropriate person or place.
+# 5. Provide contacts: If there are individuals that will need to take over your tasks, please provide this information to your supervisor. 
+# 6. Prepare the machine: The machine will need to be returned in a state ready for the next user. This includes cleaning the machine, packing it in its original box if available, etc.
+# 7. Data Security: Please ensure adherence to the data policy while transferring personal data.
+
+# Over the next few days, you will receive more information on the above points." > "$checklistFile"
+
+# Option 2: 
+# Deploy a PDF or any other file with offboarding instructions to User Desktop
+# Make sure you run the package similar to EmployeeOffboardingChecklist.pkg from github with your 
+# offboarding checklist file, which get installed at /Library/Application Support/JAMF
+
+cp /Library/Application\ Support/JAMF/EmployeeOffboardingChecklist.pdf /Users/$CURRENT_USER/Desktop 
+checklistFile='/Users/$CURRENT_USER/Desktop/EmployeeOffboardingChecklist.pdf'
 
 # Display the message with Jamf Helper and capture the button pressed
 buttonPressed=$("$jamfHelper" -windowType "$windowType" -description "$description" -button1 "$button1" -button2 "$button2" -defaultButton "$defaultButton" -cancelButton "$cancelButton" -icon "$icon" -title "$title")
+
 
 # If the Open Checklist button was pressed
 if [ "$buttonPressed" == "2" ]; then
